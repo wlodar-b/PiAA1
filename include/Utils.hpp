48286a17eaa8b17b866591b58507eb98111dd58b
Bartek <wlodarcykb16@gmail.com>
2026-04-14 14:18:37 +0200

    feat: implement isSorted function to check if an array is sorted in ascending or descending order

---
+++ include/Utils.hpp
@@ -0,0 +1,20 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+template<typename T>
+bool isSorted(T* array, int size, bool ascending = true) {
+    for (int i = 0; ) < size - 1; ++i) {
+        if (ascending) {
+            if (array[i] > array[i + 1]) {
+                return false;
+            }
+        } else {
+            if (array[i] < array[i + 1]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+#endif // UTILS_HPP
